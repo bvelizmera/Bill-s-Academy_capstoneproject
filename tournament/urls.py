95536871f72e0b59347acfdf864626fb20660427
BILL VELIZ <bvelizmera@gmail.com>
2024-03-17 13:40:29 +0000

    creation of basic nav-bar on page

--- tournament/urls.py
+++ tournament/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('tournaments/', views.TournamentList.as_view(), name='tournaments'),
-    path('', views.NewsList.as_view(), name='home')
+    path('', views.NewsList.as_view(), name='home'),
+    path('tournament/<int:pk>/', views.tournament_detail, name='tournament_detail'),
+    path('new/<int:pk>/', views.new_detail, name="new_detail"),
 ]
--- tournament/views.py
+++ tournament/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Tournament, New
 
@@ -6,8 +6,49 @@ from .models import Tournament, New
 class TournamentList(generic.ListView):
     queryset = Tournament.objects.all()
     template_name = "tournament_list.html"
-    paginated_by = 2
 
 class NewsList(generic.ListView):
-    queryset = New.objects.all()
+    queryset = New.objects.filter(status=1)
     template_name = "new_list.html"
+
+def tournament_detail(request, pk):
+    """
+    Shows tournament detials
+
+    **Context**
+    ``tournament`` instance of :model: `tournament.Tournament`
+
+    **Template:**
+
+    :template:`blog/tournament_detail.html`
+    """
+
+    queryset = Tournament.objects.all()
+    tournament = get_object_or_404(queryset, pk=pk)
+
+    return render(
+        request,
+        "tournament/tournament_detail.html",
+        {"tournament": tournament},
+    )
+
+def new_detail(request, pk):
+    """
+    Shows tournament detials
+
+    **Context**
+    ``tournament`` instance of :model: `tournament.Tournament`
+
+    **Template:**
+
+    :template:`blog/tournament_detail.html`
+    """
+
+    queryset = New.objects.all()
+    new = get_object_or_404(queryset, pk=pk)
+
+    return render(
+        request,
+        "tournament/new_detail.html",
+        {"new": new},
+    )
