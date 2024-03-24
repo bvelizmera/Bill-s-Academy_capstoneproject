2a6bfb43ee17bb7f1ae372072b57df5ecde256af
BILL VELIZ <bvelizmera@gmail.com>
2024-03-24 12:23:17 +0000

    Fixed main page layout

--- tennisacademy/settings.py
+++ tennisacademy/settings.py
@@ -37,7 +37,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-bvelizmera-billsacademy-8e56fgswj2d.ws-eu110.gitpod.io','.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://8000-bvelizmera-billsacademy-8e56fgswj2d.ws-eu110.gitpod.io','https://*.herokuapp.com']
 
