f98e637d8b655ee31298d281923060ccad9d3c49
BILL VELIZ <bvelizmera@gmail.com>
2024-03-25 03:55:21 +0000

    python validation 1

--- tournament/models.py
+++ tournament/models.py
@@ -3,19 +3,27 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from phonenumber_field.modelfields import PhoneNumberField
 
-#STATUS will define the options for the different categories
-STATUS = ((1, "8U"), (2, "10U"), (3, "11U"), (4, "12U"), (5, "14U"), (6, "16U"), (7, "18U"), (8, "Open"))
+# STATUS will define the options for the different categories
+STATUS = ((1, "8U"), (2, "10U"),
+          (3, "11U"), (4, "12U"),
+          (5, "14U"), (6, "16U"),
+          (7, "18U"), (8, "Open")
+          )
+
+
 # Create your models here.
 class WebUser(models.Model):
     """
-    Stores a single user for the website, either staff or player
+    Stores a single user for the website,
+    either staff or player
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
-    # tournament = models.ManyToManyField(Tournament, related_name="playing-tournaments", blank=True)
+    """tournament = models.ManyToManyField(Tournament,
+    related_name="playing-tournaments", blank=True)"""
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     # photo_url = models.CharField(max_length=500, blank=True)
@@ -26,8 +34,7 @@ class WebUser(models.Model):
         return f"{self.first_name} {self.last_name} - {self.user}"
 
 
-
-#This is the nournament model
+# This is the nournament model
 class Tournament(models.Model):
     name = models.CharField(max_length=100, unique=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -39,7 +46,9 @@ class Tournament(models.Model):
     prize_money = models.DecimalField(max_digits=10, decimal_places=2)
     sponsor = models.CharField(max_length=50)
     description = models.TextField(max_length=400)
-    participants = models.ManyToManyField(WebUser, related_name="participatiing", blank=True)
+    participants = models.ManyToManyField(
+        WebUser, related_name="participatiing", blank=True
+        )
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     updated_on = models.DateTimeField(auto_now=True, null=True)
     max_participants = models.IntegerField()
@@ -57,12 +66,15 @@ class Tournament(models.Model):
         # ]
 
     def __str__(self):
-        return f"{self.name} starts on {self.start_date} and ends on {self.end_date}, played on {self.surface} | sponsored by {self.sponsor}"
-    
+        return f"{self.name} starts on {self.start_date} and ends on {self.end_date},played on {self.surface} | sponsored by {self.sponsor}"
+
     def number_particitipating(self):
         return self.participants.count()
 
-OPTIONS = ((0,"Draft"), (1, "Published"))
+
+OPTIONS = ((0, "Draft"), (1, "Published"))
+
+
 class New(models.Model):
     title = models.CharField(max_length=200, unique=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -77,5 +89,4 @@ class New(models.Model):
 
     def __str__(self):
         return f"{self.title} was created on {self.created_on}. Last update {self.updated_on}"
-
-
+        
